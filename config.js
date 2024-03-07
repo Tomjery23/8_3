be9c7a3c6521ed3c54282a8bb9cc16f2df5c67d4
tomjery2k3 <tomjery2k3@gmail.com>
2024-03-07 23:50:39 +0700

    firdt commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const nameGirl = 'Em';
+const eventName = 'Chúc Mừng 8 _ 3';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 8 _ 3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'anh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
